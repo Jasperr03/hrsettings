0cd74ceea4272c270853a8403879aa9b7861ad38
Jasperr03 <jasperellevera03@gmail.com>
2024-07-01 09:18:37 +0800

    modified insert_groupsetting.php

--- schoolsett/actions/insert_groupsetting.php
+++ schoolsett/actions/insert_groupsetting.php
@@ -6,7 +6,7 @@ $grouporder = $_POST['grouporder'];
 try {
     $connHR->beginTransaction();
 
-    $insertqry = $connHR->prepare("INSERT INTO tbl_groupsetting (groupsetting, orderNo) VALUES(?,?)");
+    $insertqry = $connHR->prepare("INSERT INTO tbl_groupsetting (groupsetting, orderNo, grpstatus) VALUES(?,?,1)");
     $insertqry->execute([$groupname,$grouporder]);
 
     $connHR->commit();
